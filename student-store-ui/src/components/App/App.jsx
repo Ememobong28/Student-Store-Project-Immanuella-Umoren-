37e11b1c8a5a46903e9329799c27df4120ab37eb
Immanuella Umoren <emem@meta.com>
2023-06-21 23:28:55 -0700

    Add SideBar and Hero

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
+import Banner from "../Hero/Hero"
 import "./App.css"
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
           <Navbar />
           <Sidebar />
           <Home />
+          <Banner />
         </main>
       </BrowserRouter>
     </div>
---
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <h2>Welcome to our website!</h2>
+      {/* Additional content for the banner */}
+    </div>
+  );
+}
