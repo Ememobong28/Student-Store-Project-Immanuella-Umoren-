9a5a2d23dc6b2016ea5190c77ffc4e63a479935e
Immanuella Umoren <emem@meta.com>
2023-06-23 17:24:36 -0700

    Increment and decrement features Applied

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -2,24 +2,44 @@ import React from 'react';
 import './ProductCard.css';
 import { HiOutlineMinus, HiOutlinePlus } from "react-icons/hi";
 import { Link } from 'react-router-dom';
+import { useState} from 'react';
 
 
 export default function ProductCard({ product  , removeFromCart, addToCart}) {
-   
+  const [quantity, setQuantity] = useState(0);
+
+  const handleRemoveFromCart = () => {
+    removeFromCart(product.id, quantity);
+  };
+
+  const handleAddToCart = () => {
+    addToCart(product, quantity);
+  };
+
   return (
     <div className="product-card">
       <Link to={`product/${product.id}`}>
       <img src={product.image} alt={product.name} />
       </Link>
       <div className="quantity-controls">
-      <p>{product.name}</p>
-        <HiOutlineMinus className="sign minus" onClick={() => removeFromCart(product.id)} />
-        <span className="quantity">0</span>
-        <HiOutlinePlus className="sign plus" onClick={() => addToCart(product)} />
-      </div>
-      <p className="product-price">${product.price.toFixed(2)}</p>
-      <div className="stars">
-        <img src="/Stars.png" alt="stars" />
+        <p>{product.name}</p>
+        <HiOutlineMinus
+          className="sign minus"
+          onClick={() => {
+            if (quantity > 0) {
+              setQuantity(quantity - 1);
+              handleRemoveFromCart();
+            }
+          }}
+        />
+        <span className="quantity">{quantity}</span>
+        <HiOutlinePlus
+          className="sign plus"
+          onClick={() => {
+            setQuantity(quantity + 1);
+            handleAddToCart();
+          }}
+        />
       </div>
       
     </div>
